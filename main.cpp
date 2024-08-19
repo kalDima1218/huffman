cda0018ee170f0acf39f3dd8d0c72831c96c2bd2
Dmitry Kalugin <kalugin180806@gmail.com>
2024-08-19 10:32:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+#include <cstring>
 #include "compression.h"
 
-signed main() {
-    compression::encode("test.png"); // -> test.png.wtf
-    compression::decode("test.png.wtf"); // -> decoded_test.png
+int main(int argc, char* argv[]) {
+    if(argc != 3){
+        printf("Usage:\n-e filename\n-d filename");
+        return 0;
+    }
+    if(strcmp(argv[1], "-e") == 0){
+        compression::encode(argv[2]);
+    }
+    else if(strcmp(argv[1], "-d") == 0){
+        compression::decode(argv[2]);
+    }
+    else{
+        printf("Usage:\n-e filename\n-d filename");
+    }
+    return 0;
 }
